306e8ed3399444ff809c6ec0ed3e5ff2e5830fd7
garradefogo124 <micaelmiranda124@gmail.com>
2023-02-11 16:32:13 -0300

    Anotações e atividades utilizando funções

---
+++ lessons/ex_6/README.md
@@ -0,0 +1,5 @@
+# Exercícios para praticar a otimização na criação de funções
+
+## Resultado
+
+Cronômetro, com os botões de iniciar, pausar e zerar.
---
+++ lessons/ex_6/main.js
@@ -0,0 +1,11 @@
+function biggestNumber(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
+console.log(biggestNumber(6, 10))
+
+
+const max = (x, y) => x > y ? x : y;
+console.log(max(20, 4))
+
+const isLandscape = (height, width) => height > width;
+console.log(isLandscape(1920, 1080));
